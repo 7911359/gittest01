b7e73042a8c1b899a4fb87a52555d5be55ec081c
yang <yang@qq.com>
2019-12-08 09:30:23 +0800

    123456

--- gittest/untitled/src/test.java
+++ gittest/untitled/src/test.java
@@ -1,7 +1,10 @@
 public class test {
+
     public static void main(String[] args) {
-        System.out.println("111111111");
+
+        System.out.println('1111111111111');
         int i =1;
         i=i++;
+        i = 0;
     }
 }
